1f274134ccdc7a516694f63054726822e19db546
cyobi164 <mrgnchl090@gmail.com>
2025-03-26 23:37:10 +0900

    is saved

--- src/component/Top.jsx
+++ src/component/Top.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/photo/yc-logo.jpg";
 import "./Top.css";
 
 function Top() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="top-container">
       {/* Logo Section */}
       <div className="logo">
-        <img src={logo}/>
+        <img src={logo} />
+      </div>
+
+      {/* Hamburger Menu */}
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
       </div>
 
       {/* Navigation Bar */}
-      <ul className="nav justify-content-center">
+      <ul className={`nav ${menuOpen ? "active" : ""}`}>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#about">
+          <a className="nav-link active" href="#about">
             About me
           </a>
         </li>
