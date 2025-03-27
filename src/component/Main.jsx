ef41bb1670428659ae007723405aebc55adf245d
cyobi164 <mrgnchl090@gmail.com>
2025-03-27 15:05:50 +0900

    is saved

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Profile_image from "../assets/photo/yoshi.jpg";
 import Back_img from "../assets/photo/backimg.jpg";
+import Card01 from "../assets/photo/img01.jpg";
+import Card02 from "../assets/photo/card02.jpg";
 import "./Main.css";
 
 function Main() {
@@ -22,7 +24,8 @@ function Main() {
             「世の中にある怪我や病気を１つでも多く減らしたい」——その想いが、私の原動力です。
             神奈川県出身。幼少期から野球に打ち込み、15年間プレーを続ける中でプロを目指しトライアウトにも挑戦。
             しかし、夢半ばで挫折を経験しました。この出来事をきっかけに、「選手を支える側として貢献したい」と考え、
-            カイロプラクティックの道へ進みました。<br/>
+            カイロプラクティックの道へ進みました。
+            <br />
             さらに、身内の不幸を経験したことで、健康の大切さを改めて痛感。「病気や怪我は、未然に防ぐことができる」そう確信し、
             予防と根本改善を軸とした施術に取り組むようになりました。
             これまで3,000件以上の施術を行い、多くの方の身体の不調と向き合ってきました。
@@ -34,9 +37,50 @@ function Main() {
       </section>
 
       <section id="features" className="content-section">
-        <div className=""></div>
+        <div className="ex">
+          <h1>カイロプラクティックとは？</h1>
+          <p>
+            カイロプラクティックは、背骨や骨盤の歪みを矯正し、体の不調を改善する手技療法です。
+            主に手技を使って関節や筋肉のバランスを整え、神経の働きを良くするのが特徴。腰痛や肩こり、
+            頭痛などの症状にアプローチするだけでなく、健康維持や予防にも活用されます。
+            また、筋骨格系の障害と健康の関係を診断・治療・予防することを目的としています。
+          </p>
+        </div>
+        <div className="benifit">
+          <h1>カイロプラクティックの特徴</h1>
+          <div className="card">
+            <img src={Card01} alt="Card Image" className="card-image" />
+            <div className="card-content">
+              <h1 className="card-title">
+                その場しのぎではなく、根本原因にアプローチ一
+              </h1>
+              <p className="card-description">
+              多くの整体やマッサージは、痛みなどの症状の部位に直接アプローチをする対処療法です。
+              こういった施術の効果は一時的で、すぐに元に戻ってしまいます。
+              カイロプラティックは、一時的な症状の緩和ではなく、
+              症状の原因である「※神経の圧迫」にアプローチし、根本から健康な身体へ導きます。
+              </p>
+            </div>
+          </div>
+          
+          <div className="card">
+            <img src={Card02} alt="Card Image" className="card-image" />
+            <div className="card-content">
+              <h1 className="card-title">身体全体のバランスを整え、自然な回復力を向上</h1>
+              <p className="card-description">
+              骨格の歪みを調整し、神経の流れを正常化することで、自然治癒力を最大限に引き出します。
+              自然治癒力が高まると、治療の頻度を減らすことにつながり、
+              悩みに向き合う時間を減らすことができます。
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          
+        </div>
       </section>
-      
+
       <section id="plans" className="content-section"></section>
     </>
   );
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yoshida/', // This must match your repo name!
+  base: '/yoshida/', // This must match my repo
+  assetsInclude: ["**/*.JPG"], // this line for JPG
 })
